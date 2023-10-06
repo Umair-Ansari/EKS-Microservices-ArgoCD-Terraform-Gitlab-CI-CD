1f98ae29691a008735dee4162e6335f5722a2dd0
Muhammad Umair <mumair1992@gmail.com>
2023-10-06 10:51:30 +0400

    update code for gitlab server

--- core/main.tf
+++ core/main.tf
@@ -2,3 +2,10 @@
 module "vpc" {
   source = "./infra"
 }
+
+########## Create Gitlab Server ##########
+module "gitlab" {
+  source = "./projects/gitlab"
+  subnet_id = module.vpc.subnet_ids[1].id # l2-1a subnet
+  ssh-key = module.vpc.infra-ssh-key.id
+}
